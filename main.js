100980f84a69151e66557240b404445410fe1ff5
Yuri Kuzmichev <48134058+WorldforWorld@users.noreply.github.com>
2022-06-03 00:54:18 +0500

    feat: add new operations, fixed math bug

--- main.js
+++ main.js
@@ -1,95 +1,127 @@
-document.addEventListener('DOMContentLoaded', function(){
-	const numbers = document.querySelectorAll('.number');
-	const rotate = document.querySelectorAll('.rotate');
-	const operations = document.querySelectorAll('.operation');
-	const window = document.querySelector('.window .input');
-	const clear = document.querySelector('.clear');
-	const process = document.querySelector('.process');
-	const result = document.querySelector('.result');
+document.addEventListener('DOMContentLoaded', function() {
+  const numbers = document.querySelectorAll('.number');
+  const rotate = document.querySelector('.rotate');
+  const operations = document.querySelectorAll('.operation');
+  const clear = document.querySelector('.clear');
+  const result = document.querySelector('.result');
+  const deleteBtn = document.querySelector('.delete');
+  const square = document.querySelector('.square');
+
+  const window = document.querySelector('.window .input');
+  let process = document.querySelector('.process');
+
   let overResult = [];
   let finishRes = '';
   const clearWindow = () => {window.innerHTML = '';};
-  const clearWindowBtn = () => {window.innerHTML = ''; process.innerHTML =''};
+  const clearWindowBtn = () => {window.innerHTML = ''; process.innerHTML ='';};
 
   numbers.forEach(number => {
     number.addEventListener('click', function () {
       window.innerHTML += number.dataset.number;
-      textProcess = process.innerHTML;
     });
   });
+
   operations.forEach(operation => {
     operation.addEventListener('click', function () {
+      for (let res = 0; res < process.innerHTML.length; res++) {
+        if (process.innerHTML[res] == '=') {
+          process.innerHTML = '';
+        }
+      }
       overResult.push(Number(window.innerHTML));
       overResult.push(operation.dataset.operation);
-      process.innerHTML += Number(window.innerHTML) + operation.dataset.operation;
+      if (process.innerHTML.indexOf('sqr', process.innerHTML.length - 6) == -1) {
+        process.innerHTML += Number(window.innerHTML) + operation.dataset.operation;
+      } else {
+        process.innerHTML += operation.dataset.operation;
+      }
       clearWindow();
     });
   });
-  rotate.addEventListener('click', function () {
-    // Код
-  });
-  result.addEventListener('click', function () {
-    overResult.push(Number(window.innerHTML));
-    process.innerHTML += Number(window.innerHTML) + '=';
-    // Операции
-    for (let item = 0; item < overResult.length; item++) {
-      if(typeof overResult[item] != 'number') {
-        if (overResult[item] == 'x') {
-          operation( Number(overResult[item-1]), Number(overResult[item+1]), overResult[item] );
-          overResult.splice(item, item-1);
-          overResult[item-1] = finishRes;
-          console.log('умножение: ' + overResult);
-        }
-      }
-    }
 
-    for (let item = 0; item < overResult.length; item++) {
-      if(typeof overResult[item] != 'number') {
-        if (overResult[item] == '/') {
-          operation( Number(overResult[item-1]), Number(overResult[item+1]), overResult[item] );
-          overResult.splice(item, item-1);
-          overResult[item-1] = finishRes;
-          console.log('деление: ' + overResult);
-        }
-      }
-    }
-
-    for (let item = 0; item < overResult.length; item++) {
-      if ( overResult[item] != '/' && overResult[item] != 'x' ) {
-        if(typeof overResult[item] != 'number') {
-          operation( Number(overResult[item-1]), Number(overResult[item+1]), overResult[item] );
-          overResult.splice(item, item+1);
-          overResult[item-1] = finishRes;
-          console.log('простое сложение: ' + overResult);
-          console.log('item: ' + item);
-          item--;
-        }
-      }
-    }
-    function toFixed(value) {
-      var power = Math.pow(10, 14);
-      return String(Math.round(value * power) / power);
-    }
-    console.log('Конец: ' + overResult);
-    window.innerHTML = toFixed(finishRes);
-    process.innerHTML += toFixed(finishRes);
-  });
   clear.addEventListener('click', function () {
     finishRes = '';
     overResult = [];
     clearWindowBtn();
   });
-  function operation(a, b, operator) {
-    console.log('a: ' + a + ' b: ' + b + ' operation: ' + operator);
+
+  rotate.addEventListener('click', function () {
+    if (window.innerHTML.length != 0) {
+      window.innerHTML = Number(window.innerHTML)* (-1);
+    }
+  });
+
+  deleteBtn.addEventListener('click', function () {
+    if (window.innerHTML.length != 0) {
+      window.innerHTML = window.innerHTML.slice(0, -1);
+    }
+  });
+
+  square.addEventListener('click', function () {
+    if (window.innerHTML.length != 0) {
+      process.innerHTML += 'sqr(' + Number(window.innerHTML) + ')';
+      window.innerHTML = Number(window.innerHTML)*Number(window.innerHTML);
+    }
+  });
+
+  result.addEventListener('click', function () {
+
+    if (window.innerHTML.length == 0) {
+      finishRes = '';
+      overResult = [];
+      return clearWindowBtn();
+    }
+    overResult.push(Number(window.innerHTML));
+    if (process.innerHTML.indexOf('=') == -1) {
+      process.innerHTML += Number(window.innerHTML) + '=';
+    }
+    // Операции
+    function iterations (itemOperator) {
+      mathOperation( Number(overResult[itemOperator-1]), Number(overResult[itemOperator+1]), overResult[itemOperator], itemOperator );
+      if (itemOperator == 1) {
+        overResult.splice(itemOperator, itemOperator+1);
+      } else {
+        overResult.splice(itemOperator, itemOperator-1);
+      }
+      overResult[itemOperator-1] = finishRes;
+    }
+    for (let item = 0; item < overResult.length; item++) {
+      if(typeof overResult[item] != 'number') {
+        if (overResult[item] == '/' || overResult[item] == 'x') {
+          iterations(item);
+          item = 0;
+        }
+      }
+    }
+    
+    for (let item = 0; item < overResult.length; item++) {
+      if ( overResult[item] != '/' || overResult[item] != 'x' ) {
+        if(typeof overResult[item] != 'number') {
+          iterations(item);
+          item = 0;
+        }
+      }
+    }
+
+    function toFixed(value) {
+      var power = Math.pow(10, 14);
+      return String(Math.round(value * power) / power);
+    }
+    
+    // console.log('Конец: ' + overResult);
+    window.innerHTML = toFixed(finishRes);
+  });
+
+  function mathOperation(a, b, operator) {
     switch(operator) {
+      case 'x':
+        finishRes = a*b;
+        break;
       case '/':
-        finishRes = a/b;
         if (b === 0) {
           finishRes = 'NaN';
         }
-        break;
-      case 'x':
-        finishRes = a*b;
+        finishRes = a/b;
         break;
       case '+':
         finishRes = a + b;
@@ -104,4 +136,5 @@ document.addEventListener('DOMContentLoaded', function(){
         break;
     }
   }
+  
 });
